abbf3f2732419287bce8bded24e0f4bd90b47d9e
spartanadvetus <geekwise.eric.cesena@gmail.com>
2015-09-14 22:54:54 +0000

    once more

--- js/ex.js
+++ js/ex.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     for(var i = 2;i < games.length - 2; i++){
-        games[i].addEventListener('touchstart',function(){
+        games.addEventListener('touchstart',function(){
             alert('this is touch');
         });
     }
